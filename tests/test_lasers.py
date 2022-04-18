34d2a51d0d183eecb4fd32b7499644600539282d
kameyer226 <kentmeyer@wisc.edu>
2022-04-18 13:55:42 -0500

    update for pytest

--- tests/test_lasers.py
+++ tests/test_lasers.py
@@ -4,23 +4,27 @@ from config.definitions import ROOT_DIR
 
 """Test Requires R/W permissions in tests folder."""
 
-filepath=os.path.join(ROOT_DIR, 'tests')
+def test_laser_save_load():
+    filepath=os.path.join(ROOT_DIR, 'tests')
 
-jsonfile=os.path.join(filepath, 'las.json')
+    jsonfile=os.path.join(filepath, 'las.json')
 
-las=pc.Lasers.Lasers()
-arr1=[1500.0,3000.0,15000.0]
-las.add_frequencies(arr1)
-arr2=[0.0,-15.0, 15.0]
-las.add_angles(arr2)
-arr3=[1,-1,1]
-las.add_k_coeffs(arr3)
-arr4=[0,1,0]
-las.add_pols(arr4)
-las.change_geometry()
-las.save(jsonfile)
+    las=pc.Lasers.Lasers()
+    arr1=[1500.0,3000.0,15000.0]
+    las.add_frequencies(arr1)
+    arr2=[0.0,-15.0, 15.0]
+    las.add_angles(arr2)
+    arr3=[1,-1,1]
+    las.add_k_coeffs(arr3)
+    arr4=[0,1,0]
+    las.add_pols(arr4)
+    las.change_geometry()
+    las.save(jsonfile)
 
-las2=pc.Lasers.Lasers()
-las2.load(jsonfile)
+    las2=pc.Lasers.Lasers()
+    las2.load(jsonfile)
 
-assert (isinstance(las2, pc.Lasers.Lasers))
+    assert (isinstance(las2, pc.Lasers.Lasers))
+
+if __name__ == "__main__":
+    test_laser_save_load()
--- tests/test_phasematch.py
+++ tests/test_phasematch.py
@@ -7,82 +7,88 @@ from config.definitions import ROOT_DIR
 from sympy import S, FiniteSet, Interval, oo
 
 
+def test_pm_mcalcs():
+    filepath=os.path.join(ROOT_DIR, 'tests')
 
-filepath=os.path.join(ROOT_DIR, 'tests')
+    lay1file=os.path.join(filepath, 'CaF2_Malitson.txt')
+    lay2file=os.path.join(filepath, 'H2O_1.txt')
 
-lay1file=os.path.join(filepath, 'CaF2_Malitson.txt')
-lay2file=os.path.join(filepath, 'H2O_1.txt')
+    tkcaf2=0.02 #cm
+    tkwater=0.01 #cm
 
-tkcaf2=0.02 #cm
-tkwater=0.01 #cm
+    # generation of a IsoSample
+    samp1=pc.IsoSample.IsoSample()
+    desc="FWM cell"
+    samp1.description=desc
+    samp1.load_layer(lay1file, tkcaf2, label="caf2fw")
+    samp1.load_layer(lay2file, tkwater, label="water")
+    samp1.load_layer(lay1file, tkcaf2, label="caf2bw")
 
-# generation of a IsoSample
-samp1=pc.IsoSample.IsoSample()
-desc="FWM cell"
-samp1.description=desc
-samp1.load_layer(lay1file, tkcaf2, label="caf2fw")
-samp1.load_layer(lay2file, tkwater, label="water")
-samp1.load_layer(lay1file, tkcaf2, label="caf2bw")
+    #generation of a Lasers object.
+    las=pc.Lasers.Lasers()
+    arr1=[2700.0,1800.0,1800.0]
+    las.add_frequencies(arr1)
+    arr2=[10.0,10.0,10.0]
+    las.add_angles(arr2)
+    arr3=[1,-1,1]
+    las.add_k_coeffs(arr3)
+    arr4=[1,1,1]
+    las.add_pols(arr4)
+    las.change_geometry("boxcars")
 
-#generation of a Lasers object.
-las=pc.Lasers.Lasers()
-arr1=[2700.0,1800.0,1800.0]
-las.add_frequencies(arr1)
-arr2=[10.0,10.0,10.0]
-las.add_angles(arr2)
-arr3=[1,-1,1]
-las.add_k_coeffs(arr3)
-arr4=[1,1,1]
-las.add_pols(arr4)
-las.change_geometry("boxcars")
+    #Other test scripts show how the above objects can be saved and loaded.
+    # single point Mcalc check
+    Mlist,tklist,Tdict=pc.phasematch.m_calc(samp1,las)
+    testoutput=np.abs(Mlist[1])
+    assert (np.isclose(testoutput, 0.02978456))
 
-#Other test scripts show how the above objects can be saved and loaded.
-# single point Mcalc check
-Mlist,tklist,Tdict=pc.phasematch.m_calc(samp1,las)
-testoutput=np.abs(Mlist[1])
-assert (np.isclose(testoutput, 0.02978456))
+    # angle estimation for laser 1 in layer 2 with frequency 2600 cm-1
+    # using boxcars..test for all angles
+    angle=pc.phasematch.solve_angle(samp1,las,2,1,2600)
+    assert (np.isclose(float(angle.end), float(77.9637423)))   # angle is an Interval, Interval.end is a Sympy function
 
-# angle estimation for laser 1 in layer 2 with frequency 2600 cm-1
-# using boxcars..test for all angles
-angle=pc.phasematch.solve_angle(samp1,las,2,1,2600)
-assert (np.isclose(float(angle.end), float(77.9637423)))   # angle is an Interval, Interval.end is a Sympy function
+    # frequency estimate for a laser 1 in layer 2 to allow for phasematching
+    # with the angle in air shown in the Las object above...test for all reals >0
+    freq=pc.phasematch.solve_frequency(samp1, las, 2, 1, 10)
+    out=freq
+    assert out==Interval(0,oo)   #this may be updated if critical angles will be calculated at high freq in future
 
-# frequency estimate for a laser 1 in layer 2 to allow for phasematching
-# with the angle in air shown in the Las object above...test for all reals >0
-freq=pc.phasematch.solve_frequency(samp1, las, 2, 1, 10)
-out=freq
-assert out==Interval(0,oo)   #this may be updated if critical angles will be calculated at high freq in future
+    ####### BEGIN SECTION that can be improved next round ##########
+    Alist_in, Alist_out=pc.phasematch.calculate_absorbances(samp1,las)
+    assert (np.isclose(Alist_in[1][0], 1.180156))
+    assert (np.isclose(Alist_in[1][1], 2.973818))
 
-####### BEGIN SECTION that can be improved next round ##########
-Alist_in, Alist_out=pc.phasematch.calculate_absorbances(samp1,las)
-assert (np.isclose(Alist_in[1][0], 1.180156))
-assert (np.isclose(Alist_in[1][1], 2.973818))
+    tlist_in, tlist_out=pc.phasematch.calculate_ts(samp1,las)
+    assert (np.isclose(tlist_in[1][0], 1413.241232))
+    assert (np.isclose(tlist_out[1], 1406.194641))
 
-tlist_in, tlist_out=pc.phasematch.calculate_ts(samp1,las)
-assert (np.isclose(tlist_in[1][0], 1413.241232))
-assert (np.isclose(tlist_out[1], 1406.194641))
+    Mlistout=pc.phasematch.apply_absorbances(Mlist,Alist_in,Alist_out)
+    assert (np.isclose(Mlistout[2],5.5515857E-15))
 
-Mlistout=pc.phasematch.apply_absorbances(Mlist,Alist_in,Alist_out)
-assert (np.isclose(Mlistout[2],5.5515857E-15))
-
-Mlistout2=pc.phasematch.apply_trans(Mlist,Tdict)
-assert (np.isclose(Mlistout2[2],0.970785))
-####### END SECTION that can be improved next round ##########
+    Mlistout2=pc.phasematch.apply_trans(Mlist,Tdict)
+    assert (np.isclose(Mlistout2[2],0.970785))
+    ####### END SECTION that can be improved next round ##########
 
 
-#new lasers object:  unphasematchable geometry TSF
-las2=pc.Lasers.Lasers()
-arr1=[2700.0,1800.0,25000.0]
-las2.add_frequencies(arr1)
-arr2=[15.0,5.0,10.0]
-las2.add_angles(arr2)
-arr3=[1,1,1]
-las2.add_k_coeffs(arr3)
-arr4=[1,1,1]
-las2.add_pols(arr4)
-las2.change_geometry("boxcars")
+def test_emptyset():
+    #new lasers object:  unphasematchable geometry TSF
+    las2=pc.Lasers.Lasers()
+    arr1=[2700.0,1800.0,25000.0]
+    las2.add_frequencies(arr1)
+    arr2=[15.0,5.0,10.0]
+    las2.add_angles(arr2)
+    arr3=[1,1,1]
+    las2.add_k_coeffs(arr3)
+    arr4=[1,1,1]
+    las2.add_pols(arr4)
+    las2.change_geometry("boxcars")
 
-angle=pc.phasematch.solve_angle(samp1,las2,2,1,2600)
-out=angle
-assert out==S.EmptySet
+    angle=pc.phasematch.solve_angle(samp1,las2,2,1,2600)
+    out=angle
+    assert out==S.EmptySet
+
+
+if __name__ == "__main__":
+    test_pm_mcalcs()
+    test_emptyset()
 
