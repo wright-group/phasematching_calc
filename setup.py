cf1a9eefd525659bdecafaf6a9678d0676ca5224
kameyer226 <kentmeyer@wisc.edu>
2022-04-18 16:21:02 -0500

    added install reqs

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     name="phasematching_calc",
     packages=find_packages(),
     package_data=extra_files,
-    install_requires=["numpy", "WrightTools",  "matplotlib>=3.3.0", "sympy", "json", "distutils"],
+    install_requires=["numpy", "WrightTools",  "matplotlib>=3.3.0", "sympy", "json5", "distutils"],
     extras_require={
         "docs": docs_require,
         "dev": [
@@ -29,7 +29,7 @@ setup(
             "databroker>=1.2",
             "msgpack",
             "sympy",
-            "json"
+            "json5"
             "distutils"
         ]
         + docs_require,
